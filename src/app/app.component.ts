6c17e679da7f040678320f605d1b1409a1c45cb6
selenyel <selenyelyazilim@gmail.com>
2023-07-20 16:43:45 +0300

    Lesson 198. TD: Handling Radio Buttons
    Adding radio buttons and making them required!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
 
   defaultQuestion = 'pet'; //value of the option
   answer = ''
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
